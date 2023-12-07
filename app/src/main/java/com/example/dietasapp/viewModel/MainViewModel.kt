a34177cded006770126db3169b25dff86cf18038
Vinicius <vinijaguare@gmail.com>
2023-12-07 11:11:29 -0300

    criando ViewModel da main

--- app/src/main/java/com/example/dietasapp/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/dietasapp/viewModel/MainViewModel.kt
@@ -1,15 +1,18 @@
 package com.example.dietasapp.viewModel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.dietasapp.data.Utils
 import com.google.firebase.FirebaseNetworkException
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.tasks.await
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainViewModel : ViewModel() {
 
     private val auth = FirebaseAuth.getInstance()
+    private val db = FirebaseFirestore.getInstance()
     private var isAuth = MutableLiveData<Boolean>()
     private var msgFail = MutableLiveData<String>()
 
@@ -43,5 +46,34 @@ class MainViewModel : ViewModel() {
                 else -> "Usuario ou senha estão errados"
             }
         }
+    }
+
+    fun createUserWithEmailAndPassword(email: String, pass: String, name: String, phoneNumber: String) {
+        auth.createUserWithEmailAndPassword(email, pass)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    isAuth.value = true
+                    val userMap = hashMapOf(
+                        Utils.Firestore.FieldUserName to name,
+                        Utils.Firestore.FieldUserPhoneNumber to phoneNumber
+                    )
+                    val currUserId = auth.currentUser?.uid
+                    if (currUserId != null) {
+                        db.collection("usuarios").document(currUserId)
+                            .set(userMap)
+                            .addOnSuccessListener {
+                                Log.d("Firebase", "Salvo com sucesso")
+                            }
+                            .addOnFailureListener{
+                                Log.d("Firebase", "Problema ao salvar: ", it)
+                            }
+                    }
+
+
+
+                } else {
+                    msgFail.value = "Falha ao cadastrar usuario, ${it.exception.toString()}"
+                }
+            }
     }
 }
