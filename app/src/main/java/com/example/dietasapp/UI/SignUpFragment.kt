45fc69910707c324ae3a7e54630464d3b26289ac
Vinicius <vinijaguare@gmail.com>
2023-12-07 11:10:23 -0300

    alterando de activity para fragment

---
+++ app/src/main/java/com/example/dietasapp/UI/SignUpFragment.kt
@@ -0,0 +1,76 @@
+package com.example.dietasapp.UI
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import com.example.dietasapp.R
+import com.example.dietasapp.databinding.FragmentLoginBinding
+import com.example.dietasapp.databinding.FragmentSignUpBinding
+import com.example.dietasapp.viewModel.MainViewModel
+
+class SignUpFragment : Fragment(), View.OnClickListener {
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
+    private val mainVM: MainViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.registerButton.setOnClickListener(this)
+
+        setObserver()
+    }
+
+    fun setObserver(){
+        mainVM.getMsgFail().observe(viewLifecycleOwner) {
+            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+        }
+
+        mainVM.getIsAuth().observe(viewLifecycleOwner){
+            if(it){
+                Toast.makeText(context, "Deu certo login", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onClick(v: View) {
+        if(v.id == R.id.register_button){
+            val name = binding.userEditText.text.toString()
+            val email = binding.emailEditText.text.toString()
+            val phoneNumber = binding.phoneEditText.text.toString()
+            val pass = binding.passwordEditText.text.toString()
+            val confirmPass = binding.confirmPasswordEditText.text.toString()
+
+            if(name.isNullOrEmpty() || email.isNullOrEmpty() || phoneNumber.isNullOrEmpty() || pass.isNullOrEmpty() || confirmPass.isNullOrEmpty()){
+                Toast.makeText(context, "É nescessario preencher todos os campos", Toast.LENGTH_SHORT)
+                return
+            }
+            if(pass != confirmPass){
+                Toast.makeText(context, "As senhas estão diferentes", Toast.LENGTH_SHORT)
+                return
+            }
+//            TODO("Implementar feedback para usuario que esta sendo criado o usuario")
+
+            mainVM.createUserWithEmailAndPassword(email, pass, name, phoneNumber)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
