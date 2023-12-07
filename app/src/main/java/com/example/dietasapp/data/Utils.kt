3a5af1c44013f1d78d3f9a3e5dd895a591193589
Vinicius <vinijaguare@gmail.com>
2023-12-07 01:12:13 -0300

    Criando utils com constantes do firestore

---
+++ app/src/main/java/com/example/dietasapp/data/Utils.kt
@@ -0,0 +1,9 @@
+package com.example.dietasapp.data
+
+class Utils {
+    object Firestore{
+        const val ColectionUser = "usuarios"
+        const val FieldUserPhoneNumber = "numeroTelefone"
+        const val FieldUserName = "nome"
+    }
+}
